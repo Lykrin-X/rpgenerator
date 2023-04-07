1ec4909504a99659b8edeba5a4033bb69c5744a1
Lykrin-X <amir.jones427@gmail.com>
2023-04-06 22:43:12 -0400

    Conditional statements added to exclude/include characters

--- assets/script.js
+++ assets/script.js
@@ -13,19 +13,42 @@ var maximumPassword = 128
 
 var minimumPassword = 8
 
+var passwordOptions = [];
+var passCompleted = [];
 
 var sizePassword = prompt("Choose a number between 8 and 128 for password length:");
   if(sizePassword < minimumPassword || sizePassword > maximumPassword){
     alert("Must be a valid number between 8 and 128. Please try again.")
   }
 
-numbers = prompt("Should this password contain numbers? Click okay for yes, cancel for no.");
+var question1 = confirm("Should this password contain numbers? Click okay for yes, cancel for no.");
+  if(question1 === true){
+    passwordOptions = passwordOptions.concat(numbers)
+  }
 
-symbols = prompt("Should this password contain symbols(!,@,$)? Click okay for yes, cancel for no.");
+var question2 = confirm("Should this password contain symbols(!,@,$)? Click okay for yes, cancel for no.");
+  if(question1 === true){
+    passwordOptions = passwordOptions.concat(symbols)
+  }
 
-lettersLower = prompt("Should this password contain lowercase letters? Click okay for yes, cancel for no.")
+var question3 = confirm("Should this password contain lowercase letters? Click okay for yes, cancel for no.");
+  if(question3 === true){
+    passwordOptions = passwordOptions.concat(lettersLower)
+  }
 
-lettersUpper = prompt("Should this password contain uppercase letters? Click okay for yes, cancel for no.")
+var question4 = confirm("Should this password contain uppercase letters? Click okay for yes, cancel for no.");
+  if(question4 === true){
+    passwordOptions = passwordOptions.concat(lettersUpper)
+  }
+  console.log(passwordOptions);
+for(var i = 0; i < sizePassword; i++){
+  passCompleted.push(passwordOptions[Math.floor(Math.random()*passwordOptions.length)]);
+}
+
+console.log(passwordOptions);
+return passCompleted.join("");
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
